c8521bfb410097b4cbeb4eca7fb060f450aa2a7f
M0817-porsche <mamphomofokeng06@gmail.com>
2026-09-11 16:44:11 +0200

    Added other functions and staff blob strorage endpoints

--- CoffeNChill/Function1.cs
+++ CoffeNChill/Function1.cs
@@ -120,4 +120,80 @@ public class Function1
 
         return req.CreateResponse(HttpStatusCode.NoContent);
     }
+    [Function("GetStaffDocuments")]
+    public async Task<HttpResponseData> GetStaffDocuments(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "staff/documents")] HttpRequestData req)
+    {
+        var documents = await _staffFileService.GetAllDocumentsAsync();
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(documents);
+
+        return response;
+    }
+    [Function("UploadStaffDocument")]
+    public async Task<HttpResponseData> UploadStaffDocument(
+   [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "staff/documents/upload")] HttpRequestData req)
+    {
+        var fileName = req.Url.Query
+            .Split('&')
+            .Select(x => x.Split('='))
+            .FirstOrDefault(x => x.Length == 2 && x[0] == "fileName")?[1];
+
+        if (string.IsNullOrWhiteSpace(fileName))
+        {
+            var badResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+            await badResponse.WriteStringAsync("fileName is required.");
+            return badResponse;
+        }
+
+        fileName = Uri.UnescapeDataString(fileName);
+
+        using var memoryStream = new MemoryStream();
+        await req.Body.CopyToAsync(memoryStream);
+        memoryStream.Position = 0;
+
+        var contentType = req.Headers.TryGetValues("Content-Type", out var values)
+            ? values.FirstOrDefault() ?? "application/octet-stream"
+            : "application/octet-stream";
+
+        var document = await _staffFileService.UploadAsync(
+            memoryStream,
+            fileName,
+            contentType,
+            memoryStream.Length);
+
+        var response = req.CreateResponse(HttpStatusCode.Created);
+        await response.WriteAsJsonAsync(document);
+
+        return response;
+    }
+    [Function("DownloadStaffDocument")]
+    public async Task<HttpResponseData> DownloadStaffDocument(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "staff/documents/download/{fileName}")] HttpRequestData req,
+    string fileName)
+    {
+        try
+        {
+            var stream = await _staffFileService.DownloadAsync(fileName);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/octet-stream");
+
+            await stream.CopyToAsync(response.Body);
+
+            return response;
+        }
+        catch (Exception)
+        {
+            var notFoundResponse = req.CreateResponse(HttpStatusCode.NotFound);
+            await notFoundResponse.WriteStringAsync("Document not found.");
+
+            return notFoundResponse;
+        }
+    }
+
+
 }
+
+
--- CoffeNChill/Services/StaffFileService.cs
+++ CoffeNChill/Services/StaffFileService.cs
@@ -1,5 +1,8 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using CoffeeNChill.Models;
+using System.IO;
+using CoffeeNChill.Models;
 
 namespace CoffeeNChill.Services;
 
@@ -19,4 +22,50 @@ public class StaffFileService
     {
         await _containerClient.CreateIfNotExistsAsync();
     }
+    public async Task<StaffDocument> UploadAsync(
+    Stream fileStream,
+    string fileName,
+    string contentType,
+    long fileSize)
+    {
+        var blobClient = _containerClient.GetBlobClient(fileName);
+
+        await blobClient.UploadAsync(
+            fileStream,
+            overwrite: true);
+
+        return new StaffDocument
+        {
+            FileName = fileName,
+            ContentType = contentType,
+            FileSize = fileSize,
+            UploadedOn = DateTime.UtcNow
+        };
+    }
+    public async Task<List<StaffDocument>> GetAllDocumentsAsync()
+    {
+        var documents = new List<StaffDocument>();
+
+        await foreach (var blobItem in _containerClient.GetBlobsAsync())
+        {
+            documents.Add(new StaffDocument
+            {
+                FileName = blobItem.Name,
+                ContentType = blobItem.Properties.ContentType ?? "application/octet-stream",
+                FileSize = blobItem.Properties.ContentLength ?? 0
+            });
+        }
+
+        return documents;
+    }
+
+    public async Task<Stream> DownloadAsync(string fileName)
+    {
+        var blobClient = _containerClient.GetBlobClient(fileName);
+
+        var response = await blobClient.DownloadStreamingAsync();
+
+        return response.Value.Content;
+    }
+
 }
